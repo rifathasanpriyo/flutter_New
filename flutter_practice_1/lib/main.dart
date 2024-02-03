3c5981af2748d710b92747a16a4e4a7b8fc7efe8
Rifat Hasan Priyo <137903375+rifathosan445@users.noreply.github.com>
2024-02-03 20:53:05 +0600

    update3

--- flutter_practice_1/lib/main.dart
+++ flutter_practice_1/lib/main.dart
@@ -11,7 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Flutter Day',
       theme: ThemeData(primarySwatch: Colors.blue,brightness: Brightness.dark),
       home: homeScreen(),
