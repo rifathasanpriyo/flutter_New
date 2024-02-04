d569931572bc78329f0c5430029cd9f257830bc6
Rifat Hasan Priyo <137903375+rifathosan445@users.noreply.github.com>
2024-02-04 20:33:00 +0600

    Exit_dialog

--- flutter_practice_1/lib/main.dart
+++ flutter_practice_1/lib/main.dart
@@ -1,4 +1,11 @@
+<<<<<<< Updated upstream
+=======
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+>>>>>>> Stashed changes
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,17 +33,73 @@ class homeScreen extends StatefulWidget {
 }
 
 class _homeScreenState extends State<homeScreen> {
+<<<<<<< Updated upstream
   int _myindex=0;
   final _Pages=[
       Container(color: Colors.amber,),
       Container(color: Colors.white,),
       Container(color: Colors.red,),
+=======
+  getoutfromapp(){
+     if(Platform.isIOS)
+     {
+      try{
+        exit(0);
+      }
+      catch(e){
+      print(e);
+      }
+      
+     }
+     else{
+      try{
+        SystemNavigator.pop();
+      }
+      catch(e)
+      {
+        print(e);
+      }
+     }
+ 
+  }
+ alertDialog()
+ {
+       showDialog(context: context, builder: (_){
+         return CupertinoAlertDialog(
+          title: Text('exit dialog'),
+          content: Row(
+            children: [
+              ElevatedButton(onPressed: ()=>getoutfromapp(), child: Text('YES'),),
+              SizedBox(width: 10,),
+             ElevatedButton(onPressed: ()=>Navigator.pop(context), child: Text('NO'),),
+        
+            
+            
+            ],
+          ),
+         );
+
+       }
+       );   
+ }
+
+ bottomSheet()
+ {
+   return  showModalBottomSheet(context: context, builder: (_){
+       
+       return Container(
+        height: 200,
+       
+       );
+   });
+>>>>>>> Stashed changes
 
 
 
   ];
   @override
   Widget build(BuildContext context) {
+<<<<<<< Updated upstream
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -65,6 +128,39 @@ class _homeScreenState extends State<homeScreen> {
            Expanded(child: _Pages[_myindex]),
          ],
        ),
+=======
+    return WillPopScope(
+      onWillPop: (){
+        alertDialog();
+        return Future.value(false);
+      },
+      child: Scaffold(
+         
+          appBar: AppBar(
+            title: Text("Flutter"),
+            backgroundColor: Colors.red,
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+              ElevatedButton(
+                  onPressed: ()=>alertDialog()
+              , child: Text("alert_dialog"),
+              
+              ),
+             ElevatedButton(
+                  onPressed: ()=>bottomSheet()
+              , child: Text("Bottom_Sheet"),
+              
+              ),
+            
+              ],
+            ),
+          ),
+      
+>>>>>>> Stashed changes
       ),
     );
   }
