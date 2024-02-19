da27df061394f1e2bb4427e4dd92fd2631a22ff2
Rifat Hasan Priyo <137903375+rifathosan445@users.noreply.github.com>
2024-02-19 20:43:32 +0600

    image_picker

--- flutter_practice_1/lib/main.dart
+++ flutter_practice_1/lib/main.dart
@@ -1,66 +1,80 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter_practice_1/yes_button.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Day',
       //theme: ThemeData(primarySwatch: Colors.blue,brightness: Brightness.dark),
-      home: homeScreen(),
+      home: HomeScreen(),
     );
   }
 }
 
-class homeScreen extends StatefulWidget {
-  const homeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  State<homeScreen> createState() => _homeScreenState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _homeScreenState extends State<homeScreen> {
- var gender=['Male','Female','Baal'];
-  var index='Male';
+class _HomeScreenState extends State<HomeScreen> {
+  final ImagePicker _picker = ImagePicker();
+  XFile? image;
+  List<XFile>? images;
+
+   fontCamera() async {
+    image = await _picker.pickImage(source: ImageSource.camera);
+    setState(() {});
+  }
+  fromgallary()async
+  {
+       image=await _picker.pickImage(source: ImageSource.gallery); 
+       setState(() {
+         
+       });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: AppBar(
-         title: Text("Flutter"),
+      appBar: AppBar(
+        title: Text("ok"),
         backgroundColor: Colors.red,
-       ),
-       body: DropdownButton(
-        value: index,
-        items: gender.map(
-          (value) {
-            return DropdownMenuItem(
-              value:value,
-              child: Text(value,style: TextStyle(color: Colors.black),));
-          }
-          
-          ).toList(),
-          
-           
-        onChanged: (value){
-          setState(() {
-            index=value!;
-          });
-          
+      ),
+      body: Center(
+      
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (image != null)
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.file(
+                  File(image!.path),
+                  width: 200,
+                ),
+              )
+            else
+              Icon(Icons.person_2_outlined),
 
-        }
+              IconButton(onPressed: ()=>fontCamera(), icon: Icon(Icons.camera)),
+
+
+              IconButton(onPressed: ()=>fromgallary(), icon: Icon(Icons.browse_gallery_outlined)),
+          ],
+          
         ),
-      
-      
-      
-       
-   );
+      ),
+    );
   }
 }
