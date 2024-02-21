1ef4726294a306f7ab90b389a25ef47f910ddb79
Rifat Hasan Priyo <137903375+rifathosan445@users.noreply.github.com>
2024-02-22 00:46:44 +0600

    music_player

--- flutter_practice_1/lib/main.dart
+++ flutter_practice_1/lib/main.dart
@@ -1,21 +1,30 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  
   Widget build(BuildContext context) {
     return MaterialApp(
+      
       debugShowCheckedModeBanner: false,
       title: 'Flutter Day',
       //theme: ThemeData(primarySwatch: Colors.blue,brightness: Brightness.dark),
       home: HomeScreen(),
+      
     );
   }
 }
@@ -28,52 +37,167 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final ImagePicker _picker = ImagePicker();
-  XFile? image;
-  List<XFile>? images;
+  bool change=false;
+  final player = AudioPlayer();
+  bool isPlaying=false;
+  Duration duration=Duration.zero;
+  Duration position=Duration.zero;
+/*setaudio()async{
+await player.play(AssetSource('djsong.mp3'));
 
-   fontCamera() async {
-    image = await _picker.pickImage(source: ImageSource.camera);
-    setState(() {});
   }
-  fromgallary()async
-  {
-       image=await _picker.pickImage(source: ImageSource.gallery); 
+  @override
+  void initState() {
+    setaudio();
+    super.initState();
+  }
+  @override
+  void dispose() {
+   player.dispose();
+    super.dispose();
+  }*/
+   String formatTime(int seconds) {
+    return '${(Duration(seconds: seconds))}'.split('.')[0].padLeft(8, '0');
+  }
+ 
+  @override
+  void initState() {
+    super.initState();
+    player.onPlayerStateChanged.listen((state) { 
        setState(() {
-         
+         isPlaying=state==PlayerState.playing;
        });
+    
+    }
+    );
+
+    player.onDurationChanged.listen((newDuration) { 
+    setState(() {
+      duration=newDuration;
+    });
+
+    }
+    );
+    
+   player.onPositionChanged.listen((newPosition) { 
+    setState(() {
+      position=newPosition;
+    });
+   }
+   );
+
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("ok"),
-        backgroundColor: Colors.red,
-      ),
-      body: Center(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue,brightness:change? Brightness.dark:Brightness.light),
+      home: Scaffold(
+       // backgroundColor: change ?Colors.white:Colors.black,
+        
+        drawer: Drawer(
+           width: 170,
+           
+           child: ListView(
+            children: [ 
+              SizedBox(height:30),
+              ListTile(
+                title: Icon(Icons.home,),
+                
+              ),
+              ListTile(
+                title: Icon(Icons.favorite,),
+                
+              ),
+              ListTile( 
+                title: Icon(Icons.settings),
+                
+              ),
+              Switch(value: change,onChanged: (newvalue) {
+                setState(() {
+                  change=newvalue;
+                });
+              },)
+            ],
+           ), 
       
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (image != null)
-              ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Image.file(
-                  File(image!.path),
-                  width: 200,
-                ),
-              )
-            else
-              Icon(Icons.person_2_outlined),
-
-              IconButton(onPressed: ()=>fontCamera(), icon: Icon(Icons.camera)),
-
-
-              IconButton(onPressed: ()=>fromgallary(), icon: Icon(Icons.browse_gallery_outlined)),
-          ],
-          
         ),
+        appBar: AppBar(
+          title: Center(child: Text("P L A Y E R",style: TextStyle(fontWeight: FontWeight.bold),)),
+          backgroundColor: Colors.grey,
+        ),
+         body: Center(
+           child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+             children: [ 
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Image.asset('asset/images/letme.png',height: 300,),
+                  ),
+                ),
+                //SizedBox(height: 30,),
+              Slider(min:0,max: duration.inSeconds.toDouble(),
+              value: position.inSeconds.toDouble(), 
+              onChanged: (value){
+                position = Duration(seconds: value.toInt());
+                player.seek(position);
+                player.resume();
+              }
+              ),
+              Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [ 
+                     Text(formatTime(position.inSeconds),style: TextStyle(fontWeight: FontWeight.bold),),
+                     Text(formatTime((duration-position).inSeconds),style: TextStyle(fontWeight: FontWeight.bold),),
+      
+                    ],
+                  ),
+      
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  
+                  
+                ElevatedButton.icon(onPressed: (){}, icon: Icon(Icons.skip_previous_sharp), label: Text('')),
+                  FloatingActionButton(onPressed: ()async{
+                             
+                  if(player.state==PlayerState.playing)
+                  {
+                    await player.pause();
+                    
+                    setState(() {
+                      
+                    });
+                  }
+                  else
+                  {
+                   await player.play(AssetSource('letmesong.mp3'));
+                  
+                    setState(() {
+                      
+                    });
+                  }
+                  },
+                  child: player.state==PlayerState.playing ? Icon(Icons.pause_circle_rounded,size: 50,):Icon(Icons.play_circle_rounded,size: 50,),
+                 
+                  
+                  ),
+                  ElevatedButton.icon(onPressed: (){}, icon: Icon(Icons.skip_next), label: Text('')),
+      
+                  
+                ],
+              ),
+              SizedBox(height: 20,),
+              Text('Let Me Down Slowly',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),)
+             ],
+             
+           ),
+         ),
       ),
     );
   }
