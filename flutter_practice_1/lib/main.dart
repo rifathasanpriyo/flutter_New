fce712dda90fb9cd30acd1b45aa0d578e963eadb
Rifat Hasan Priyo <137903375+rifathosan445@users.noreply.github.com>
2024-02-26 17:20:44 +0600

    video_player

--- flutter_practice_1/lib/main.dart
+++ flutter_practice_1/lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:audioplayers/audioplayers.dart';
+import 'package:appinio_video_player/appinio_video_player.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,15 +17,12 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   @override
-  
   Widget build(BuildContext context) {
     return MaterialApp(
-      
       debugShowCheckedModeBanner: false,
       title: 'Flutter Day',
       //theme: ThemeData(primarySwatch: Colors.blue,brightness: Brightness.dark),
       home: HomeScreen(),
-      
     );
   }
 }
@@ -37,168 +35,36 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  bool change=false;
-  final player = AudioPlayer();
-  bool isPlaying=false;
-  Duration duration=Duration.zero;
-  Duration position=Duration.zero;
-/*setaudio()async{
-await player.play(AssetSource('djsong.mp3'));
-
-  }
+  late CachedVideoPlayerController _videoPlayerController;
+  late CustomVideoPlayerController _customVideoPlayerController;
   @override
   void initState() {
-    setaudio();
+    _videoPlayerController = CachedVideoPlayerController.network(
+        'https://sample-videos.com/video321/mp4/720/big_buck_bunny_720p_1mb.mp4')
+      ..initialize().then((value) => setState(() {}));
+    _customVideoPlayerController = CustomVideoPlayerController(
+        context: context,
+        videoPlayerController: _videoPlayerController,
+        customVideoPlayerSettings: CustomVideoPlayerSettings(
+        placeholderWidget: Center( child: CircularProgressIndicator(),)
+
+        ));
     super.initState();
   }
   @override
   void dispose() {
-   player.dispose();
+    // TODO: implement dispose
+    _customVideoPlayerController.dispose();
     super.dispose();
-  }*/
-   String formatTime(int seconds) {
-    return '${(Duration(seconds: seconds))}'.split('.')[0].padLeft(8, '0');
-  }
- 
-  @override
-  void initState() {
-    super.initState();
-    player.onPlayerStateChanged.listen((state) { 
-       setState(() {
-         isPlaying=state==PlayerState.playing;
-       });
-    
-    }
-    );
-
-    player.onDurationChanged.listen((newDuration) { 
-    setState(() {
-      duration=newDuration;
-    });
-
-    }
-    );
-    
-   player.onPositionChanged.listen((newPosition) { 
-    setState(() {
-      position=newPosition;
-    });
-   }
-   );
-
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.blue,brightness:change? Brightness.dark:Brightness.light),
-      home: Scaffold(
-       // backgroundColor: change ?Colors.white:Colors.black,
-        
-        drawer: Drawer(
-           width: 170,
-           
-           child: ListView(
-            children: [ 
-              SizedBox(height:30),
-              ListTile(
-                title: Icon(Icons.home,),
-                
-              ),
-              ListTile(
-                title: Icon(Icons.favorite,),
-                
-              ),
-              ListTile( 
-                title: Icon(Icons.settings),
-                
-              ),
-              Switch(value: change,onChanged: (newvalue) {
-                setState(() {
-                  change=newvalue;
-                });
-              },)
-            ],
-           ), 
-      
-        ),
-        appBar: AppBar(
-          title: Center(child: Text("P L A Y E R",style: TextStyle(fontWeight: FontWeight.bold),)),
-          backgroundColor: Colors.grey,
-        ),
-         body: Center(
-           child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-             children: [ 
-                Align(
-                  alignment: Alignment.topCenter,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Image.asset('asset/images/letme.png',height: 300,),
-                  ),
-                ),
-                //SizedBox(height: 30,),
-              Slider(min:0,max: duration.inSeconds.toDouble(),
-              value: position.inSeconds.toDouble(), 
-              onChanged: (value){
-                position = Duration(seconds: value.toInt());
-                player.seek(position);
-                player.resume();
-              }
-              ),
-              Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [ 
-                     Text(formatTime(position.inSeconds),style: TextStyle(fontWeight: FontWeight.bold),),
-                     Text(formatTime((duration-position).inSeconds),style: TextStyle(fontWeight: FontWeight.bold),),
-      
-                    ],
-                  ),
-      
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  
-                  
-                ElevatedButton.icon(onPressed: (){}, icon: Icon(Icons.skip_previous_sharp), label: Text('')),
-                  FloatingActionButton(onPressed: ()async{
-                             
-                  if(player.state==PlayerState.playing)
-                  {
-                    await player.pause();
-                    
-                    setState(() {
-                      
-                    });
-                  }
-                  else
-                  {
-                   await player.play(AssetSource('letmesong.mp3'));
-                  
-                    setState(() {
-                      
-                    });
-                  }
-                  },
-                  child: player.state==PlayerState.playing ? Icon(Icons.pause_circle_rounded,size: 50,):Icon(Icons.play_circle_rounded,size: 50,),
-                 
-                  
-                  ),
-                  ElevatedButton.icon(onPressed: (){}, icon: Icon(Icons.skip_next), label: Text('')),
-      
-                  
-                ],
-              ),
-              SizedBox(height: 20,),
-              Text('Let Me Down Slowly',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),)
-             ],
-             
-           ),
-         ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('V I D E '),
       ),
+      body: CustomVideoPlayer(customVideoPlayerController: _customVideoPlayerController),
     );
   }
 }
